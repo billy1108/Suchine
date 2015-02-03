a8eed38b024cac8e8c91248ac32779df061087c6
BillyCaballero <billy.11080@gmail.com>
2015-02-02 23:02:07 -0500

    implement settings

--- app/src/main/java/com/caballero/billy/sunshine/ForecastFragment.java
+++ app/src/main/java/com/caballero/billy/sunshine/ForecastFragment.java
@@ -1,5 +1,8 @@
 package com.caballero.billy.sunshine;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,8 +11,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -42,8 +47,7 @@ public class ForecastFragment extends Fragment {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
-            FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute(this, "94043");
+            updateWeather();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -57,13 +61,35 @@ public class ForecastFragment extends Fragment {
 
         ListView listItemView = (ListView) rootView.findViewById(R.id.listview_forescast);
 
-        ArrayList<String> items = new ArrayList<String>();
-        items.add("17 Fake Street");
-        items.add("Phoney town");
-        items.add("Makebelieveland");
-
-        mForestcastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forescast, R.id.list_item_forescast_textview, items);
+        mForestcastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forescast, R.id.list_item_forescast_textview, new ArrayList<String>());
         listItemView.setAdapter(mForestcastAdapter);
+
+
+        listItemView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String forecast = mForestcastAdapter.getItem(position);
+                //Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent intentDetail =  new Intent(getActivity(), DetailActivity.class);
+                intentDetail.putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intentDetail);
+            }
+        });
         return rootView;
+    }
+
+    private void updateWeather() {
+        FetchWeatherTask weatherTask = new FetchWeatherTask();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location = prefs.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+        weatherTask.execute(this, location);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
     }
 }
